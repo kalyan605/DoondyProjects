6a79a84a6b2e6eb33f3f2032d0a8ef72e5c1bff4
kalyan605 <86517899+kalyan605@users.noreply.github.com>
2021-07-17 17:57:18 +0530

    Add files via upload

---
+++ FileUpload1.module.scss.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+require("./FileUpload1.module.css");
+const styles = {
+  button: 'button_5dd0ab92',
+  label: 'label_5dd0ab92'
+};
+
+export default styles;
+/* tslint:enable */
